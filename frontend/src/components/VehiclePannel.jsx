cb27add30f2113a35c593b1d70076ba5df57e4e1
rev0212 <ra6857@srmist.edu.in>
2024-12-02 21:43:44 +0530

    Added User UI Components

--- frontend/src/components/VehiclePannel.jsx
+++ frontend/src/components/VehiclePannel.jsx
@@ -9,8 +9,11 @@ const VehiclePanel = (props) => {
             }} className='p-1 text-center w-[93%] absolute top-0'><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
             <h3 className='text-2xl font-semibold mb-5'>Choose a Vehicle</h3>
             
-            {/* Car section */}
-            <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+            {/* Car section */} 
+            <div onClick={()=>{
+                props.setIsConfirmedRidePanelOpen(true);
+                props.setIsVehiclePanelOpen(false);
+            }} className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
                 <img className='h-10' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="UberGo" />
                 <div className='ml-2 w-1/2'>
                     <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
@@ -21,7 +24,10 @@ const VehiclePanel = (props) => {
             </div>
 
             {/* Moto section */}
-            <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+            <div  onClick={()=>{
+                props.setIsConfirmedRidePanelOpen(true);
+                props.setIsVehiclePanelOpen(false);
+            }} className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
                 <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_638,w_956/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="Moto" />
                 <div className='-ml-2 w-1/2'>
                     <h4 className='font-medium text-base'>Moto <span><i className="ri-user-3-fill"></i>1</span></h4>
@@ -32,7 +38,10 @@ const VehiclePanel = (props) => {
             </div>
 
             {/* Auto section */}
-            <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+            <div  onClick={()=>{
+                props.setIsConfirmedRidePanelOpen(true);
+                props.setIsVehiclePanelOpen(false);
+            }} className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
                 <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="UberAuto" />
                 <div className='ml-2 w-1/2'>
                     <h4 className='font-medium text-base'>UberAuto <span><i className="ri-user-3-fill"></i>3</span></h4>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,15 +2,25 @@ import React, { useState, useRef, useEffect } from 'react';
 import gsap from 'gsap';
 import LocationSearchPannel from '../components/LocationSearchPannel';
 import VehiclePanel from '../components/VehiclePannel';
+import ConfirmedRidePannel from '../components/ConfirmedRidePannel';
+import LookingForDriverPannel from '../components/LookingForDriverPannel';
+import WaitingForDriverPannel from '../components/WaitingForDriverPannel';
 
 const Home = () => {
   const [pickup, setPickup] = useState('');
   const [destination, setDestination] = useState('');
+
   const [isPannelOpen, setIsPannelOpen] = useState(false);
   const [isVehiclePanelOpen, setIsVehiclePanelOpen] = useState(false);
+  const [isConfirmedRidePanelOpen, setIsConfirmedRidePanelOpen] = useState(false);
+  const [isLookingForDriverPanelOpen, setIsLookingForDriverPanelOpen] = useState(false)
+  const [isWaitingForDriverPanelOpen, setisWaitingForDriverPanelOpen] = useState(false)
 
   const pannelRef = useRef(null);
   const vehiclePanelRef = useRef(null);
+  const confirmedRidePanelRef = useRef(null);
+  const lookingForDriverPanelRef = useRef(null);
+  const waitingForDriverPanelRef = useRef(null)
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -34,17 +44,56 @@ const Home = () => {
     }
   }, [isVehiclePanelOpen]);
 
+  // GSAP animation for the confirmed ride panel
+  useEffect(() => {
+    if (isConfirmedRidePanelOpen) {
+      gsap.to(confirmedRidePanelRef.current, { transform: 'translateY(0)' });
+    } else {
+      gsap.to(confirmedRidePanelRef.current, { transform: 'translateY(100%)' });
+    }
+  }, [isConfirmedRidePanelOpen]);
+
+
+  //GSAP animation for the Looking for driver pannel
+  useEffect(() => {
+    if (isLookingForDriverPanelOpen) {
+      gsap.to(lookingForDriverPanelRef.current, { transform: 'translateY(0)' });
+    } else {
+      gsap.to(lookingForDriverPanelRef.current, { transform: 'translateY(100%)' });
+    }
+  }, [isLookingForDriverPanelOpen]);
+
+
+  //GSAP animation for Waiting For Driver Pannel
+
+  useEffect(() => {
+    if (isWaitingForDriverPanelOpen) {
+      gsap.to(waitingForDriverPanelRef.current, { transform: 'translateY(0)' });
+    } else {
+      gsap.to(waitingForDriverPanelRef.current, { transform: 'translateY(100%)' });
+    }
+  }, [isWaitingForDriverPanelOpen]);
+
+
   return (
-    <div className='h-screen relative'>
-      <img className='w-16 absolute left-5 top-5' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s' alt="Logo" />
+    <div className='h-screen relative overflow-hidden'>
+      <img
+        className='w-16 absolute left-5 top-5'
+        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s'
+        alt='Logo'
+      />
       <div className='h-screen w-screen'>
-        <img className='h-full w-full object-cover ' src="https://cdn.theatlantic.com/thumbor/9o5OxuWCAaGrA19A-LW0MlKj_u8=/0x48:1231x740/976x549/media/img/mt/2017/04/IMG_7105/original.png" alt="Background" />
+        <img
+          className='h-full w-full object-cover'
+          src='https://cdn.theatlantic.com/thumbor/9o5OxuWCAaGrA19A-LW0MlKj_u8=/0x48:1231x740/976x549/media/img/mt/2017/04/IMG_7105/original.png'
+          alt='Background'
+        />
       </div>
       <div className='flex flex-col justify-end h-screen absolute top-0 w-full'>
         <div className='h-[30%] p-5 bg-white relative'>
           <h4 className='text-2xl font-semibold'>Find a Trip</h4>
           <form onSubmit={submitHandler}>
-            <div className="line absolute h-16 w-1 top-[30%] left-8 bg-gray-800"></div>
+            <div className='line absolute h-16 w-1 top-[30%] left-8 bg-gray-800'></div>
             <input
               className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3'
               type='text'
@@ -64,10 +113,37 @@ const Home = () => {
           </form>
         </div>
         <div ref={pannelRef} className='h-0 bg-white'>
-          <LocationSearchPannel setIsPanelOpen={setIsPannelOpen} setIsVehiclePanelOpen={setIsVehiclePanelOpen} />
+          <LocationSearchPannel
+            setIsPanelOpen={setIsPannelOpen}
+            setIsVehiclePanelOpen={setIsVehiclePanelOpen}
+          />
         </div>
-        <div ref={vehiclePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
-          <VehiclePanel setIsVehiclePanelOpen={setIsVehiclePanelOpen} />
+        <div
+          ref={vehiclePanelRef}
+          className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'
+        >
+          <VehiclePanel
+            setIsVehiclePanelOpen={setIsVehiclePanelOpen}
+            setIsConfirmedRidePanelOpen={setIsConfirmedRidePanelOpen}
+          />
+        </div>
+        <div
+          ref={confirmedRidePanelRef}
+          className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'
+        >
+          <ConfirmedRidePannel setIsLookingForDriverPanelOpen={setIsLookingForDriverPanelOpen} setIsConfirmedRidePanelOpen={setIsConfirmedRidePanelOpen} />
+        </div>
+        <div
+          ref={lookingForDriverPanelRef}
+          className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'
+        >
+          <LookingForDriverPannel/>
+        </div>
+        <div
+          ref={waitingForDriverPanelRef}
+          className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'
+        >
+          <WaitingForDriverPannel isWaitingForDriverPanelOpen={isWaitingForDriverPanelOpen}/>
         </div>
       </div>
     </div>
